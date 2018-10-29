07b83ac05de7cc390dae79416625ad45f4c5c8f7
pipnina <james.ytedmonds@gmail.com>
2018-10-29 22:12:24 +0000

    Changes to input handling functions
    Requires further work... Some shit's gone real fucked up in there.

--- NText/Editing.js
+++ NText/Editing.js
@@ -3,36 +3,63 @@ console.log("EDITOR IS ALIVE!");
 //WHERE THE EDITOR'S RAW TEXT SAUCE IS KEPT
 TextStore = function()
 {
-	this.store;
+	_this = this;
+	_this.store = "JavaScript Was Made By A Horse";
 	
-	this.addchars = function(input)
+	_this.addchars = function(input)
 	{
-		this.store += input;
-		console.log(this.store);
+		_this.store += input;
+		console.log(_this.store);
 	}
+	
+	console.log(_this.store);
 };
 
 //HANDLES AND MANAGES ACTIVE KEYS (I.E. KNOWS WHAT KEYS ARE CURRENTLY HELD DOWN)
 InputHandler = function()
 {
-	this.activeKeys = [];
+	_this = this;
+	_this.activeKeys = [];
 	
-	this.newkeypress = function(newkey)
+	_this.newkeypress = function(newkey)
 	{
-		this.activeKeys.push(newkey);
-		console.log(this.activeKeys);
-	}
-	
-	this.newkeyrelease = function(newkey)
-	{
+		console.log(newkey);
 		
+		if(_this.activeKeys.forEach(function(key, index, array)
+		{
+			if(key.key == newkey.key)
+			{
+				console.log(newkey.key + " is already in the active keys list");
+				return "no";
+			}
+		}) != "no")
+		{
+			_this.activeKeys.push(newkey);
+		}
+		console.log(_this.activeKeys);
 	}
-}
 
+	_this.newkeyrelease = function(newkey)
+	{
+		_this.activeKeys.forEach(function(key, index, array)
+		{
+			if(key.key == newkey.key)
+			{
+				array.splice(index, 1);
+				console.log(array);
+				return "Removed "+key.key;
+			}
+		});
+	}
+};
 
+//End of definitions, now they're made to work
 TextBox	= document.getElementById("TextField");
 Store	= new TextStore();
 Input	= new InputHandler();
+
+Store.addchars("NNNNNNNNNNN");
+Store.addchars("OOOOOOOOOOO");
 
 console.log(TextBox.clientHeight);
 
@@ -49,4 +76,5 @@ TextBox.addEventListener("focus", function(event)
 	console.log("FOCUS GAINED");
 });
 
-TextBox.addEventListener("keydown", Input.newkeypress(newkey));
+TextBox.addEventListener("keydown", Input.newkeypress);
+TextBox.addEventListener("keyup", Input.newkeyrelease);
